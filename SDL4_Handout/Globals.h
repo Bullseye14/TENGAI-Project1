04ac0a2c58a3747bfc5237e91fd4308343d0b98d
Bullseye14 <36154277+Bullseye14@users.noreply.github.com>
2018-03-27 13:18:55 +0200

    SCREEN WIDTH AND HEIGHT CHANGED

--- SDL4_Handout/Globals.h
+++ SDL4_Handout/Globals.h
@@ -19,8 +19,8 @@ typedef unsigned int uint;
 
 // Configuration -----------
 #define SCREEN_SIZE 3
-#define SCREEN_WIDTH 384
-#define SCREEN_HEIGHT 224
+#define SCREEN_WIDTH 700
+#define SCREEN_HEIGHT 525
 #define WIN_FULLSCREEN 0
 #define WIN_FULLSCREEN_DESKTOP 0
 #define WIN_BORDERLESS 0
