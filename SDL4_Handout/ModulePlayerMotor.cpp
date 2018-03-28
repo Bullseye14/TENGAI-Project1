439969b487721876913271965ad8ca0f6ff473d8
ponspack9 <ospoga9@gmail.com>
2018-03-28 13:11:26 +0200

    Shoot ready to be implemented

---
+++ SDL4_Handout/ModulePlayerMotor.cpp
@@ -0,0 +1,26 @@
+#include "ModulePlayerMotor.h"
+#include "ModuleTextures.h"
+#include "Globals.h"
+
+
+ModulePlayerMotor::ModulePlayerMotor()
+{
+
+}
+
+
+ModulePlayerMotor::~ModulePlayerMotor()
+{
+}
+bool ModulePlayerMotor::Start()
+{
+	return true;
+}
+
+update_status ModulePlayerMotor::Update()
+{
+	return UPDATE_CONTINUE;
+}
+void ModulePlayerMotor::Shoot()
+{
+}
---
+++ SDL4_Handout/ModulePlayerMotor.h
@@ -0,0 +1,36 @@
+#ifndef __ModulePlayerMotor_H__
+#define __ModulePlayerMotor_H__
+
+#include "Module.h"
+#include "Animation.h"
+
+enum bulletType
+{
+	bullet,
+	secondBullet,
+	bomb,
+	specialAttack
+
+};
+
+class ModulePlayerMotor : public Module
+{
+public:
+	ModulePlayerMotor();
+	~ModulePlayerMotor();
+
+	bool Start();
+	update_status Update();
+	void Shoot();
+
+
+public:
+	SDL_Texture* graphics = nullptr;
+	Animation bullet;
+	Animation secondBullet;
+	Animation bomb;
+	Animation specialAttack;
+
+};
+
+#endif
