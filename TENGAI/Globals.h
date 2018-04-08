58a1c61d97724903382e999ada7b18b1f41ac76c
Bullseye14 <36154277+Bullseye14@users.noreply.github.com>
2018-04-08 12:28:05 +0200

    changed screen height and width

--- TENGAI/Globals.h
+++ TENGAI/Globals.h
@@ -21,9 +21,9 @@ enum update_status
 typedef unsigned int uint;
 
 // Configuration -----------
-#define SCREEN_SIZE 3
-#define SCREEN_WIDTH 233
-#define SCREEN_HEIGHT 163
+#define SCREEN_SIZE 2
+#define SCREEN_WIDTH 320
+#define SCREEN_HEIGHT 224
 #define WIN_FULLSCREEN 0
 #define WIN_FULLSCREEN_DESKTOP 0
 #define WIN_BORDERLESS 0
